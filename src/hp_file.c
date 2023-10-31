be55a0e1f83798f157437538117c4ca7563da0ba
BronzeKennen <sotoskand3@gmail.com>
2023-11-01 00:06:21 +0200

    incomplete piece of trash function we all going to jail

--- src/hp_file.c
+++ src/hp_file.c
@@ -30,7 +30,9 @@ int HP_CreateFile(char *fileName){
     HP_info *data = test; //MOVE TO AN HP_INFO POINTER  
     data->type = Heap; // SET DATA FOR HP_INFO
     data->last_block_id = 0;
+    
     BF_Block_SetDirty(block); //SET DIRTY AND UNPIN TO WRITE TO DISK
+    
     CALL_BF(BF_UnpinBlock(block));
     BF_Block_Destroy(&block);
     CALL_BF(BF_CloseFile(file_desc));
@@ -38,6 +40,7 @@ int HP_CreateFile(char *fileName){
 }
 
 HP_info* HP_OpenFile(char *fileName, int *file_desc){
+
     BF_Block *block;
     BF_Block_Init(&block);
     BF_OpenFile(fileName,file_desc);
@@ -65,6 +68,8 @@ int HP_CloseFile(int file_desc,HP_info* hp_info ){
         BF_ErrorCode code = BF_UnpinBlock(block);
     
     }
+
+    BF_CloseFile(file_desc);
 }
 
 int HP_InsertEntry(int file_desc,HP_info* hp_info, Record record){
@@ -72,23 +77,18 @@ int HP_InsertEntry(int file_desc,HP_info* hp_info, Record record){
     BF_Block_Init(&block);
 
     BF_GetBlock(file_desc,hp_info->last_block_id,block);
-    HP_block_info *blockInfo = (HP_block_info*)block + ((BF_BLOCK_SIZE / 4) - sizeof(HP_block_info)); 
+    char *data = BF_Block_GetData(block);
 
-    if(blockInfo->num_of_records == RECORDS_PER_BLOCK || hp_info->last_block_id == 0) {
-        //ALLOCATE NEW BLOCK
-        BF_Block *newBlock =  AllocateAndUpdate(file_desc, hp_info);
-        blockInfo->next_block = newBlock; 
-    } 
+    HP_block_info* blockInfo = data + BF_BLOCK_SIZE - sizeof(HP_block_info);
+    printf("%d\n",blockInfo->num_of_records);
+    if(hp_info->last_block_id == 0 || blockInfo->num_of_records == RECORDS_PER_BLOCK) {
+        
+        BF_AllocateBlock(file_desc,block);
+        HP_block_info blockInfo = {0,NULL};
+        char* blockData = BF_Block_GetData(block);
+        memcpy(blockData + BF_BLOCK_SIZE - sizeof(HP_block_info),&blockInfo,sizeof(HP_block_info));
     
-    BF_GetBlock(file_desc,hp_info->last_block_id,block);
-    blockInfo = (HP_block_info*)block + ((BF_BLOCK_SIZE / 4) - sizeof(HP_block_info));
-    void* data = BF_Block_GetData(block);
-    Record *rec = data;
-    rec[blockInfo->num_of_records] = record;
-    blockInfo->num_of_records++;
-    
-    printf("%d \n",blockInfo->num_of_records); 
-    BF_Block_SetDirty(block);
+    }
     return 0;
 }
 
@@ -96,14 +96,14 @@ int HP_GetAllEntries(int file_desc,HP_info* hp_info, int value){
     return -1;
 }
 
-BF_Block* AllocateAndUpdate(int file_desc, HP_info* hp_info) {
+// BF_Block* AllocateAndUpdate(int file_desc, HP_info* hp_info) {
     
-    BF_Block *block;
-    HP_block_info blockInfo = {0, NULL};
+//     BF_Block *block;
+//     HP_block_info blockInfo = {0, NULL};
     
-    BF_Block_Init(&block); 
-    BF_AllocateBlock(file_desc, block);
-    memcpy((HP_block_info*)block + ((BF_BLOCK_SIZE / 4) - sizeof(HP_block_info)),&blockInfo,sizeof(HP_block_info));
-    hp_info->last_block_id++;
-    return block;
-}
+//     BF_Block_Init(&block); 
+//     BF_AllocateBlock(file_desc, block);
+//     memcpy((char*)block + ((BF_BLOCK_SIZE) - sizeof(HP_block_info)),&blockInfo,sizeof(HP_block_info));
+//     hp_info->last_block_id++;
+//     return block;
+// }
